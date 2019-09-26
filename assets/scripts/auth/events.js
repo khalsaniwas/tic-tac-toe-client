79972e04d359cde070880f12606c5217803a86e1
khalsaniwas <honeysaini21@gmail.com>
2019-09-25 22:17:20 -0400

    Add sign-up feature

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,18 @@
+'use strict'
+const getFormFields = require('../../../lib/get-form-fields')
+const api = require('./api.js')
+const ui = require('./ui.js')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const formData = getFormFields(form)
+  api.signUp(formData)
+    .then(ui.onSignUpSuccess)
+    .catch(ui.onSignUpFailure)
+}
+
+module.exports = {
+  onSignUp
+}
