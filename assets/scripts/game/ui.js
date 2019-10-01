7024bddecc8da2006c7aeda80716f148155e139f
khalsaniwas <honeysaini21@gmail.com>
2019-09-30 21:44:14 -0400

    Show winner

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -34,16 +34,39 @@ const onTurnSuccess = function (gameData) {
   for (let i = 0; i < store.game.cells.length; i++) {
     $(`#${i}`).text(store.game.cells[i])
   }
+  checkWin()
+
   if (store.turn === 'x') {
     store.turn = 'o'
   } else {
     store.turn = 'x'
   }
+
   successMessage('Turn: Player ' + store.turn)
 }
 
 const onTurnFailure = function (gameData) {
   failureMessage('Failed to change turn')
+}
+
+const checkWin = function () {
+  if (store.game.cells[0] !== '' && store.game.cells[0] === store.game.cells[1] && store.game.cells[1] === store.game.cells[2]) {
+    errorMessage('Winner is: ' + store.game.cells[0])
+  } else if (store.game.cells[3] !== '' && store.game.cells[3] === store.game.cells[4] && store.game.cells[4] === store.game.cells[5]) {
+    errorMessage('Winner is: ' + store.game.cells[3])
+  } else if (store.game.cells[6] !== '' && store.game.cells[6] === store.game.cells[7] && store.game.cells[7] === store.game.cells[8]) {
+    errorMessage('Winner is: ' + store.game.cells[6])
+  } else if (store.game.cells[0] !== '' && store.game.cells[0] === store.game.cells[3] && store.game.cells[3] === store.game.cells[6]) {
+    errorMessage('Winner is: ' + store.game.cells[0])
+  } else if (store.game.cells[1] !== '' && store.game.cells[1] === store.game.cells[4] && store.game.cells[4] === store.game.cells[7]) {
+    errorMessage('Winner is: ' + store.game.cells[1])
+  } else if (store.game.cells[2] !== '' && store.game.cells[2] === store.game.cells[5] && store.game.cells[5] === store.game.cells[8]) {
+    errorMessage('Winner is: ' + store.game.cells[2])
+  } else if (store.game.cells[0] !== '' && store.game.cells[0] === store.game.cells[4] && store.game.cells[4] === store.game.cells[8]) {
+    errorMessage('Winner is: ' + store.game.cells[0])
+  } else if (store.game.cells[2] !== '' && store.game.cells[2] === store.game.cells[4] && store.game.cells[4] === store.game.cells[6]) {
+    errorMessage('Winner is: ' + store.game.cells[2])
+  }
 }
 
 module.exports = {
